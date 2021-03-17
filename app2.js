cbfb0cea18ca33112f3ac7392936b8b09630a068
CHANDRA BHANU TATA <TATACHANDRABHANU1141@GMAIL.COM>
2021-03-17 14:51:11 -0500

    previous table updated in app2.js

--- app2.js
+++ app2.js
@@ -11,10 +11,13 @@ const server =
 
       res.write('<html> <head> <title> Served </title> </head>')
       res.write('<body>')
-      res.write('Content \n')
-      res.write('More content \n')
-      res.write('Hello World')    
-      res.end('</body></html>')
+      res.write('<table>')
+      res.write('<tr><th> location </th><th> No.hours </th><th> Amount </th></tr>')
+      res.write('<tr><td> Adhi Yogi Statue</td><td> 4</td><td> free</td></tr>')  
+      res.write('<tr><td> goa</td><td> 8 </td><td> 6000rs</td></tr>')  
+      res.write('<tr><td> Ooty</td><td> 12</td><td> 12000rs</td></tr>')
+      res.write('<tr><td> beach</td><td>  24</td><td> 15000rs</td></tr>')
+      res.end('</table></body></html>')
       //Close the response
     }                           
 )
